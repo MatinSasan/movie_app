8213df6a7c88c1692dda67b66ec622282121eab6
Matin Sasanpour <matin.sasan@gmail.com>
2020-09-29 07:51:50 +0330

    only rating left

--- src/App.js
+++ src/App.js
@@ -7,21 +7,48 @@ import { APIURL, IMGPATH, SEARCHAPI } from './config';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const getMovies = async () => {
-    const moviesRep = await fetch(APIURL);
+  const getMovies = async api => {
+    const moviesRep = await fetch(api);
     const _movies = await moviesRep.json();
     setMovies(_movies.results);
   };
 
   useEffect(() => {
-    getMovies();
+    getMovies(APIURL);
   }, []);
 
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCHAPI + searchTerm);
+      setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div>
-      {movies.length > 0 && movies.map(movie => <Movie key={movie.id} />)}
-    </div>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className='search'
+            type='text'
+            placeholder='search...'
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className='movie-container'>
+        {movies.length > 0 &&
+          movies.map(movie => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
 
-const Movie = () => <div className='movie'>Movie stuff</div>;
+import { IMGPATH } from '../config.js';
+
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  <div className='movie'>
+    <img src={IMGPATH + poster_path} alt={title} />
+    <div className='movie-info'>
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
+    </div>
+    <div className='movie-over'>
+      <h2>Overview:</h2>
+      <p>{overview}</p>
+    </div>
+  </div>
+);
 export default Movie;
